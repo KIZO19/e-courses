6fb270522e8754d19093ff10f463aebf23058659
KIZO19 <100475773+KIZO19@users.noreply.github.com>
2024-03-22 13:03:44 +0300

    changes

--- view/pages/files/navbar.php
+++ view/pages/files/navbar.php
@@ -44,9 +44,9 @@
                                     <a href="single.html" class="dropdown-item">Blog Detail</a>
                                 </div>
                             </div>
-                            <a href="contact.html" class="nav-item nav-link">Contact</a>
+                            <a href="contact" class="nav-item nav-link">Contact</a>
                         </div>
-                        <a class="btn btn-primary py-2 px-4 ml-auto d-none d-lg-block" href="">Join Now</a>
+                        <a class="btn btn-primary py-2 px-4 ml-auto d-none d-lg-block" href="http://localhost/e-courses/view\pages\users\pages\examples\login">Join Now</a>
                     </div>
                 </nav>
             </div>
